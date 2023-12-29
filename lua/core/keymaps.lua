25d5db0ba092a37ce2da6efd99a48d785828e38b
ht3aa <ht3aa2001@gmail.com>
2023-12-29 16:09:51 +0300

    Alt+first hand row for numbers

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -2,7 +2,46 @@ lvim.keys.normal_mode["<S-l>"] = ":bnext<CR>"
 lvim.keys.normal_mode["<S-h>"] = ":bprevious<CR>"
 lvim.keys.normal_mode["<C-n>"] = ":%s/\\<<C-r><C-w>\\>//gI<Left><Left><Left>"
 lvim.keys.visual_mode["p"] = '"_dP'
-lvim.keys.visual_mode["J"] = ":m '>+1<CR>gv=gv"
 lvim.keys.visual_mode["K"] = ":m '<-2<CR>gv=gv"
+lvim.keys.visual_mode["J"] = ":m '>+1<CR>gv=gv"
 lvim.keys.insert_mode["jj"] = "<ESC>"
 
+lvim.keys.normal_mode["<A-a>"] = "1"
+lvim.keys.insert_mode["<A-a>"] = "1"
+lvim.keys.visual_mode["<a-a>"] = "1"
+
+lvim.keys.normal_mode["<A-s>"] = "2"
+lvim.keys.insert_mode["<A-s>"] = "2"
+lvim.keys.visual_mode["<a-s>"] = "2"
+
+lvim.keys.normal_mode["<A-d>"] = "3"
+lvim.keys.insert_mode["<A-d>"] = "3"
+lvim.keys.visual_mode["<a-d>"] = "3"
+
+lvim.keys.normal_mode["<A-f>"] = "4"
+lvim.keys.insert_mode["<A-f>"] = "4"
+lvim.keys.visual_mode["<a-f>"] = "4"
+
+lvim.keys.normal_mode["<A-g>"] = "5"
+lvim.keys.insert_mode["<A-g>"] = "5"
+lvim.keys.visual_mode["<a-g>"] = "5"
+
+lvim.keys.normal_mode["<A-h>"] = "6"
+lvim.keys.insert_mode["<A-h>"] = "6"
+lvim.keys.visual_mode["<a-h>"] = "6"
+
+lvim.keys.normal_mode["<A-j>"] = "7"
+lvim.keys.insert_mode["<A-j>"] = "7"
+lvim.keys.visual_mode["<a-j>"] = "7"
+
+lvim.keys.normal_mode["<A-k>"] = "8"
+lvim.keys.insert_mode["<A-k>"] = "8"
+lvim.keys.visual_mode["<a-k>"] = "8"
+
+lvim.keys.normal_mode["<A-l>"] = "9"
+lvim.keys.insert_mode["<A-l>"] = "9"
+lvim.keys.visual_mode["<a-l>"] = "9"
+
+lvim.keys.normal_mode["<A-;>"] = "0"
+lvim.keys.insert_mode["<A-;>"] = "0"
+lvim.keys.visual_mode["<a-;>"] = "0"
