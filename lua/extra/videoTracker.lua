83a866e422d1c12fc76a02f22c60bb5d9b9c0c4f
ht3aa <ht3aa2001@gmail.com>
2024-03-21 20:29:13 +0300

    Adding last commit info to the name of video file

--- lua/extra/videoTracker.lua
+++ lua/extra/videoTracker.lua
@@ -67,22 +67,52 @@ local function tmux_window_exists(window_name)
   return false
 end
 
+
+local function execute_command(command)
+  local handle = io.popen(command)
+
+  if not handle then return nil end
+
+  local result = handle:read("*a")
+  handle:close()
+  return result
+end
+
+local function get_last_commit_info()
+  local gitLogCommand = "git log -1 --pretty=format:'%h %s'"
+  return execute_command(gitLogCommand)
+end
+
+
+
+
 function StartVideoTracker()
   createYearDirectoryIfNeeded()
   createMonthDirectoryIfNeeded()
   createDayDirectoryIfNeeded()
 
+  local lastCommitInfo = get_last_commit_info()
+
+  if lastCommitInfo == nil then
+    lastCommitInfo = "null"
+  end
+
   if not tmux_window_exists("videoTracker") then
     run_terminal_command_in_tmux(
       "ffmpeg -video_size 1366x768 -framerate 10 -f x11grab -i :1 -crf 40 -preset veryfast " .. videosPath ..
-      os.date("%Y") .. "/" .. os.date("%m") .. "/" .. os.date("%d") .. "/" .. os.date("%H-%M-%S") .. ".mkv")
+      os.date("%Y") ..
+      "/" ..
+      os.date("%m") ..
+      "/" .. os.date("%d") .. "/" .. os.date("%H-%M-%S") .. "-" .. lastCommitInfo:gsub("%s", "-") .. ".mkv")
   else
     print("videoTracker window is already open.")
   end
 end
+
 function StopVideoTracker()
   run_terminal_command_in_tmux("pkill ffmpeg")
 end
+
 
 vim.cmd([[autocmd VimEnter * lua StartVideoTracker()]])
 vim.cmd([[autocmd VimLeave * lua StopVideoTracker()]])
