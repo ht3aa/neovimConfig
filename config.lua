595b6496b1bd7e24740aefa94b7f82dff9f57fdb
ht3aa <ht3aa2001@gmail.com>
2024-04-11 22:54:07 +0300

    fix: harpoon

--- config.lua
+++ config.lua
@@ -10,7 +10,6 @@ reload "core.nvimTree"
 reload "extra"
 
 
-
 vim.notify = require("notify")
 vim.cmd([[autocmd VimLeave * lua SaveTypingTracker()]])
 vim.cmd([[autocmd TextChanged,TextChangedI * lua Update_typing_productivity_seconds()]])
--- lua/extra/plugins.lua
+++ lua/extra/plugins.lua
@@ -2,7 +2,21 @@ lvim.plugins = {
   {
     "ThePrimeagen/harpoon",
     branch = "harpoon2",
-    dependencies = { "nvim-lua/plenary.nvim" }
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      local harpoon = require("harpoon")
+
+      -- REQUIRED
+      harpoon:setup()
+      -- REQUIRED
+
+      vim.keymap.set("n", "<leader>oa", function() harpoon:list():add() end)
+      vim.keymap.set("n", "<leader>ot", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+
+      vim.keymap.set("n", "<leader>og", function() harpoon:list():select(1) end)
+      vim.keymap.set("n", "<leader>od", function() harpoon:list():select(2) end)
+      vim.keymap.set("n", "<leader>of", function() harpoon:list():select(3) end)
+    end
   },
   {
     "mg979/vim-visual-multi",
